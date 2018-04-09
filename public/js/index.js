c02175c78580bdfaf07f195ed5a358de1b6ad04f
Jon Pelc <thisisjonpelc@gmail.com>
2018-04-09 12:44:51 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,14 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to Server");
-
-  socket.emit("createMessage", {
-    from: "Jon",
-    text: "Don't bother me fool!"
-  });
 });
 
 socket.on("disconnect", function() {
   console.log("Disconnected from server.");
+});
+
+socket.on("newEmail", function(email) {
+  console.log("New email", email);
 });
 
 socket.on("newMessage", function(message){
